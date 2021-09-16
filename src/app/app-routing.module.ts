02b91240572e7debc79ef6b4b48f6ddbb4db26d3
luis carlos <luiscepeda269@gmail.com>
2021-09-16 07:17:56 -0500

    archivo de frontend clase miercoles

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+//componentes
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+
+const routes: Routes = [
+  {path: '', component: ListarProductosComponent },
+  {path: 'crear-producto', component: CrearProductoComponent},
+  {path: 'editar-producto/:id', component: ListarProductosComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
